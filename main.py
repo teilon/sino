e62b4e885b402953bdb8a33d6ec2d2980a2cf214
teilon <teiloncw@gmail.com>
2019-04-09 03:44:12 +0600

    merge reports

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from calc import use_best_stations
 from calcaroma import get_data_by_aromas_move
 from lost import get_report
 
+from tmp import tmp
+
 
 def main():
 
-    use_best_stations()
-    # get_report()
+    # use_best_stations()
+
+    tmp()
 
 
 if __name__ == "__main__":
