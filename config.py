1ef7d449fdf574609ab9e0399ba6994ab4362e92
teilon <teiloncw@gmail.com>
2019-04-15 19:37:28 +0600

    add config file

---
+++ config.py
@@ -0,0 +1,8 @@
+def db_config():
+    return 'dbname={} user={} password={} port={} host={}'.format(
+        'dbmarket',
+        'torn',
+        'dtmorgan',
+        '5432',
+        '185.143.173.37'
+    )
